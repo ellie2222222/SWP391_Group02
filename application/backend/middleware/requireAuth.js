61103ce2f86d7de3a17be0af76e28b5a233c20f1
DKBT31 <thuanbakhac@gmail.com>
2024-05-29 19:50:51 +0700

    Added Admin delete user

--- application/backend/middleware/requireAuth.js
+++ application/backend/middleware/requireAuth.js
@@ -1,26 +1,30 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
-
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 const requireAuth = async (req, res, next) => {
-    const { authorization } = req.headers
+  const { authorization } = req.headers;
 
-    if (!authorization) {
-        return res.status(401).json({error: 'Authorization token required'})
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorization token required' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+
+    const user = await User.findById(_id).select('_id role');
+    
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
     }
 
-    const token = authorization.split(' ')[1]
-
-    try {
-        const {_id} = jwt.verify(token, process.env.SECRET)
-        console.log(_id)
-
-        req.user = await User.findOne({_id}).select('_id')
-        
-        next()
-    } catch (error) {
-        res.status(403).json({error: "Request is not authorized"})
-    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: 'Request is not authorized' });
+  }
 }
 
-module.exports = requireAuth 
+module.exports = requireAuth;
--- application/backend/routes/user.js
+++ application/backend/routes/user.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const { loginUser, signupUser } = require('../controllers/userController')
+const { loginUser, signupUser, deleteUser} = require('../controllers/userController')
+const requireAuth = require('../middleware/requireAuth')
+const requireAdmin = require('../middleware/requireAdmin')
 
 const userRoutes = express.Router()
 
 userRoutes.post('/login', loginUser)
 
 userRoutes.post('/signup', signupUser)
+
+userRoutes.delete('/delete/:id', requireAuth, requireAdmin, deleteUser);
 
 module.exports = userRoutes
