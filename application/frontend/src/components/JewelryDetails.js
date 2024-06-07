c64e523fe3417926f9804c666cab52f00a3a5806
Tran Nam <namtran09022004@gmail.com>
2024-06-07 21:25:40 +0700

    Update UI

--- application/frontend/src/components/JewelryDetails.js
+++ application/frontend/src/components/JewelryDetails.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Box, Typography, Button, CircularProgress } from '@mui/material';
+import { Container, Box, Typography, Button, CircularProgress, styled } from '@mui/material';
 import axios from 'axios';
+
+const CustomButton1 = styled(Button)({
+  outlineColor: '#000',
+  backgroundColor:'#b48c72',
+  color: '#fff',
+  width: '100%',
+  fontSize: '1rem',
+  marginTop: '20px',
+  '&:hover': {
+    color: '#b48c72', // Thay đổi màu chữ khi hover
+    backgroundColor: 'transparent',
+  },
+});
 
 const JewelryDetails = () => {
   const { id } = useParams();
@@ -32,9 +45,9 @@ const JewelryDetails = () => {
     <Container>
       <Box display="flex" flexDirection="row" padding="40px 0">
         <Box flex={1} paddingRight="20px">
-          <img src='https://www.tierra.vn/files/halo-A7tL5Eltco.webp' alt={product.name} style={{ width: '100%' }} />
+          <img src='https://www.tierra.vn/files/halo-A7tL5Eltco.webp' alt={product.name} style={{ width: '100%', height:'100%' }} />
         </Box>
-        <Box flex={1}>
+        <Box flex={1} display="flex" flexDirection="column" justifyContent="space-between">
           <Typography variant="h4" component="h1">{product.name}</Typography>
           <Typography variant="h5">{product.price} USD</Typography>
           <Typography variant="body1">SKU: {product.gemstone}</Typography>
@@ -45,9 +58,9 @@ const JewelryDetails = () => {
           <Typography variant="body1">Type of Stone: {product.typeOfStone}</Typography>
           <Typography variant="body1">Diamond Size: {product.diamondSize}</Typography>
           <Typography variant="body1">Ring Size: {product.ringSize}</Typography>
-          <Button variant="contained" color="primary" style={{ marginTop: '20px' }}>
+          <CustomButton1 variant="contained" color="primary" style={{ marginTop: '20px' }}>
             ORDER NOW
-          </Button>
+          </CustomButton1>
         </Box>
       </Box>
     </Container>
