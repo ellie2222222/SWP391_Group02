36fba8f415b8dad1dd2145422cf106151b424f03
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-06-12 17:33:20 +0700

    adjust

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -7,14 +7,8 @@ import About from "./pages/About";
 import Details from "./pages/Details";
 import Signup from "./pages/Signup";
 import Admin from "./pages/Admin";
-<<<<<<< HEAD
-import Order from "./pages/Order";
-import BlogContents from "./pages/BlogContents";
-import AdminRoute from "./routes/routes";
-=======
 import BlogContents from "./pages/BlogContents";
 import Request from "./pages/Request";
->>>>>>> c30390d3048cf0f7831d422f8063e229542b56a8
 function App() {
   return (
     <div className="App">
@@ -28,15 +22,8 @@ function App() {
         <Route path='/product/:id' element={<Details />}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
-        <Route path="/admin" element={<AdminRoute></AdminRoute>} >
-        <Route path="/admin" element={<Admin/>} ></Route>
-        </Route>
-        <Route path="/order/:id" element={<Order />} />
-=======
         <Route path="/admin" element={<Admin />} />
         <Route path="/request" element={<Request />} />
->>>>>>> c30390d3048cf0f7831d422f8063e229542b56a8
       </Routes>
     </div>
   );
