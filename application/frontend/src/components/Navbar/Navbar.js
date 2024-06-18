9b42e77021ca2e64262c0a1b91cc404c047f59bf
Souf <leminhtam31104@gmail.com>
2024-06-18 09:01:42 +0700

    adjust

--- application/frontend/src/components/Navbar/Navbar.js
+++ application/frontend/src/components/Navbar/Navbar.js
@@ -1,88 +1,88 @@
-import React, {  useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { styled } from '@mui/system';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import { Link } from 'react-router-dom';
-import logo from '../assets/imgs/logo.png';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import SearchIcon from '@mui/icons-material/Search';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import useAuth from '../../hooks/useAuthContext';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { styled } from "@mui/system";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import { Link } from "react-router-dom";
+import logo from "../assets/imgs/logo.png";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import useAuth from "../../hooks/useAuthContext";
 const CustomAppBar = styled(AppBar)({
-  backgroundColor: '#fff',
-  color: '#000',
+  backgroundColor: "#fff",
+  color: "#000",
 });
 
 const CustomButton = styled(Button)({
-  color: '#000',
-  textTransform: 'uppercase',
-  width: '100%',
-  height: '100%', // Chiếm toàn bộ chiều cao của Grid item
-  fontSize: '1.6rem',
-  fontWeight: '400',
-  '&:hover': {
-    backgroundColor: 'transparent', // Xóa hiệu ứng viền mặc định
-    color: '#b48c72', // Thay đổi màu chữ khi hover
-    borderRadius: '0',
+  color: "#000",
+  textTransform: "uppercase",
+  width: "100%",
+  height: "100%", // Chiếm toàn bộ chiều cao của Grid item
+  fontSize: "1.6rem",
+  fontWeight: "400",
+  "&:hover": {
+    backgroundColor: "transparent", // Xóa hiệu ứng viền mặc định
+    color: "#b48c72", // Thay đổi màu chữ khi hover
+    borderRadius: "0",
   },
-  '&.active': {
-    backgroundColor: 'transparent', // Xóa hiệu ứng viền mặc định
-    color: '#b48c72', // Thay đổi màu chữ khi active
-    borderRadius: '0',
+  "&.active": {
+    backgroundColor: "transparent", // Xóa hiệu ứng viền mặc định
+    color: "#b48c72", // Thay đổi màu chữ khi active
+    borderRadius: "0",
   },
 });
 
-const CustomListItem = styled(ListItem)({  
-  textTransform: 'uppercase',
-  '&:hover': {
-    backgroundColor: 'transparent', // Xóa hiệu ứng viền mặc định
-    color: '#b48c72', // Thay đổi màu chữ khi hover
+const CustomListItem = styled(ListItem)({
+  textTransform: "uppercase",
+  "&:hover": {
+    backgroundColor: "transparent", // Xóa hiệu ứng viền mặc định
+    color: "#b48c72", // Thay đổi màu chữ khi hover
   },
 });
 const CustomLink = styled(Link)({
-  textDecoration: 'none',
-  color: 'inherit',
+  textDecoration: "none",
+  color: "inherit",
 });
 
 const CustomIconButton = styled(IconButton)({
-  color: '#000',
-  fontSize: '2.6rem',
-  
-  '&:hover': {
-    backgroundColor: 'transparent',
-    color: '#b48c72',
+  color: "#000",
+  fontSize: "2.6rem",
+
+  "&:hover": {
+    backgroundColor: "transparent",
+    color: "#b48c72",
   },
 });
 const CustomTextField = styled(TextField)({
-  width: '200px', // Increase the width by 100px
-  borderRadius: '30px', // Add border radius to round the corners
+  width: "200px", // Increase the width by 100px
+  borderRadius: "30px", // Add border radius to round the corners
   variant: "outlined",
-  padding:'0',
-  '& fieldset': {
-    borderRadius: '30px',
+  padding: "0",
+  "& fieldset": {
+    borderRadius: "30px",
   },
 });
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [anchorEl, setAnchorEl] = useState(null);
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const { user, logout } = useAuth();
 
@@ -105,8 +105,14 @@ const Navbar = () => {
   return (
     <CustomAppBar position="static">
       <Toolbar>
-        <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-          <CustomLink to='/'><img src={logo} alt="Diamond Logo" style={{ height: '50px', cursor: 'pointer' }} /></CustomLink>
+        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+          <CustomLink to="/">
+            <img
+              src={logo}
+              alt="Diamond Logo"
+              style={{ height: "50px", cursor: "pointer" }}
+            />
+          </CustomLink>
         </Box>
         {isMobile ? (
           <>
@@ -125,77 +131,98 @@ const Navbar = () => {
             >
               <List>
                 <CustomListItem button onClick={handleDrawerClose}>
-                  <CustomLink to='/products'>
+                  <CustomLink to="/products">
                     <ListItemText primary="Products" />
                   </CustomLink>
                 </CustomListItem>
                 <CustomListItem button onClick={handleDrawerClose}>
-                  <CustomLink to='/blogs'>
+                  <CustomLink to="/blogs">
                     <ListItemText primary="Blogs" />
                   </CustomLink>
                 </CustomListItem>
                 <CustomListItem button onClick={handleDrawerClose}>
-                  <CustomLink to='/aboutus'>
+                  <CustomLink to="/aboutus">
                     <ListItemText primary="About Us" />
                   </CustomLink>
                 </CustomListItem>
                 <CustomListItem button onClick={handleDrawerClose}>
                   <ListItemText primary="Sales" />
-                  </CustomListItem>
+                </CustomListItem>
                 <CustomListItem button onClick={handleDrawerClose}>
-                  <CustomLink to='/warranties'>
+                  <CustomLink to="/warranties">
                     <ListItemText primary="Warranty" />
                   </CustomLink>
                 </CustomListItem>
                 <CustomListItem button onClick={handleDrawerClose}>
                   <ListItemIcon>
-                    <SearchIcon style={{ fontSize: '1.6rem' }} />
+                    <SearchIcon style={{ fontSize: "1.6rem" }} />
                   </ListItemIcon>
                 </CustomListItem>
                 <CustomListItem button onClick={handleDrawerClose}>
                   <ListItemIcon>
-                    <AccountCircleIcon style={{ fontSize: '1.6rem' }} />
+                    <AccountCircleIcon style={{ fontSize: "1.6rem" }} />
                   </ListItemIcon>
                 </CustomListItem>
               </List>
             </Drawer>
           </>
         ) : (
-          <Box sx={{ flexGrow: 2, display: 'flex', justifyContent: 'space-between' }}>
-           
-              <Grid container spacing={0} sx={{ height: '64px' }}> {/* Chiều cao của AppBar */}
-                <Grid item xs>
-                  <Link to='/products'>
-                    <CustomButton>Products</CustomButton>
-                  </Link>
-                </Grid>
-                <Grid item xs>
-                  <Link to='/blogs'>
-                    <CustomButton>Blogs</CustomButton>
-                  </Link>
-                </Grid>
-                <Grid item xs>
-                  <Link to='/aboutus'>
-                    <CustomButton>About Us</CustomButton>
-                  </Link>
-                </Grid>
-                <Grid item xs>
-                <Link to='/sales'>
-                  <CustomButton>Sales</CustomButton>
-                  </Link>
-                </Grid>
-                <Grid item xs>
-                <Link to='/warranties'>
-                  <CustomButton>Warranty</CustomButton>
-                  </Link>
-                </Grid>
+          <Box
+            sx={{
+              flexGrow: 2,
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Grid container spacing={0} sx={{ height: "64px" }}>
+              {" "}
+              {/* Chiều cao của AppBar */}
+              <Grid item xs>
+                <Link to="/products">
+                  <CustomButton>Products</CustomButton>
+                </Link>
               </Grid>
+              <Grid item xs>
+                <Link to="/blogs">
+                  <CustomButton>Blogs</CustomButton>
+                </Link>
+              </Grid>
+              <Grid item xs>
+                <Link to="/aboutus">
+                  <CustomButton>About Us</CustomButton>
+                </Link>
+              </Grid>
+              <Grid item xs>
+                <Link to="/sales">
+                  <CustomButton>Sales</CustomButton>
+                </Link>
+              </Grid>
+              <Grid item xs>
+                <Link to="/warranties">
+                  <CustomButton>Warranty</CustomButton>
+                </Link>
+              </Grid>
+              {user && user.role === "user" && (
+                <Grid item xs>
+                  <Link to="/request">
+                    <CustomButton>Custom Request</CustomButton>
+                  </Link>
+                </Grid>
+              )}
+              {user &&
+                (user.role === "manager" || user.role === "sale_staff") && (
+                  <Grid item xs>
+                    <Link to="/requests/customer-requests-view">
+                      <CustomButton>Customer Requests</CustomButton>
+                    </Link>
+                  </Grid>
+                )}
+            </Grid>
             {/* Thêm icon login và thanh search */}
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
               <CustomTextField
-               
                 size="normal"
-                label='Search...'
+                label="Search..."
                 value={search}
                 onChange={(event) => {
                   setSearch(event.target.value);
@@ -203,18 +230,17 @@ const Navbar = () => {
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
-                      <CustomIconButton color='inherit'>
-                        <SearchIcon fontSize='2.6rem' />
+                      <CustomIconButton color="inherit">
+                        <SearchIcon fontSize="2.6rem" />
                       </CustomIconButton>
                     </InputAdornment>
                   ),
                 }}
-
               />
               {user ? (
                 <>
                   <CustomIconButton color="inherit" onClick={handleMenuOpen}>
-                    <AccountCircleIcon fontSize='2.6rem' />
+                    <AccountCircleIcon fontSize="2.6rem" />
                   </CustomIconButton>
                   <Menu
                     anchorEl={anchorEl}
@@ -222,15 +248,20 @@ const Navbar = () => {
                     onClose={handleMenuClose}
                   >
                     <MenuItem onClick={logout}>Logout</MenuItem>
-                    <MenuItem component={Link} to="/profile" onClick={handleMenuClose}>
+                    <MenuItem
+                      component={Link}
+                      to="/profile"
+                      onClick={handleMenuClose}
+                    >
                       Profile
                     </MenuItem>
                   </Menu>
-z                </>
+                  z{" "}
+                </>
               ) : (
-                <Link to='/login'>
+                <Link to="/login">
                   <CustomIconButton color="inherit">
-                    <AccountCircleIcon fontSize='2.6rem' />
+                    <AccountCircleIcon fontSize="2.6rem" />
                   </CustomIconButton>
                 </Link>
               )}
