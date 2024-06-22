f41037cbfa6830cd509e4fecc3f9a15a87d87db5
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-06-22 09:47:46 +0700

    adjust baseUrl

--- application/frontend/src/utils/axiosInstance.js
+++ application/frontend/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:4000/api',
-    baseURL: 'https://frontend-chk2.onrender.com',
+    baseURL: 'https://backend-j9ne.onrender.com/api',
 });
 
 axiosInstance.interceptors.request.use(
