37507b8ef29cde9416c5ffe87a1515fa3206d959
DKBT31 <thuanbakhac@gmail.com>
2024-06-28 15:42:59 +0700

    fix login signup

--- application/backend/models/userModel.js
+++ application/backend/models/userModel.js
@@ -34,47 +34,53 @@ const userSchema = new Schema( {
 
 userSchema.statics.signup = async function(username, email, password, phone_number, address) {
     if (!username || !email || !password || !phone_number || !address) {
-        throw Error('All fields must be filled')   
+        throw Error('All fields must be filled');
     }
 
     if (!validator.isEmail(email)) {
-        throw Error('Email is not valid')
+        throw Error('Email is not valid');
     }
     if (!validator.isStrongPassword(password)) {
-        throw Error('Password is not strong enough')
+        throw Error('Password is not strong enough');
     }
-    
-    const exists = await this.findOne({ email })
 
+    const exists = await this.findOne({ email });
     if (exists) {
-        throw Error('Email already exists')
+        throw Error('Email already exists');
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-    const user = this.create({username, email, password: hash, role: 'user', phone_number, address})
-    
-    return user
-}
+    const user = await this.create({
+        username,
+        email,
+        password: hash,
+        role: 'user',
+        phone_number,
+        address
+    });
+
+    return user;
+};
 
 userSchema.statics.login = async function(email, password) {
     if (!email || !password) {
-        throw Error('All fields must be filled')
+        throw Error('All fields must be filled');
     }
 
-    const user = await this.findOne({ email })
+    const user = await this.findOne({ email });
     if (!user) {
-        throw Error('Email not found')
+        throw Error('Email not found');
     }
 
-    const match = await bcrypt.compare(password, user.password)
+    const match = await bcrypt.compare(password, user.password);
     if (!match) {
-        throw Error('Incorrect password')
+        throw Error('Incorrect password');
     }
 
-    return user
-}
+    return user;
+};
 
 userSchema.methods.checkPassword = async function(password) {
     return await bcrypt.compare(password, this.password);
@@ -82,8 +88,14 @@ userSchema.methods.checkPassword = async function(password) {
 
 userSchema.pre('save', async function(next) {
     if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
+        // Check if the password is already hashed
+        const isHashed = this.password.startsWith('$2b$');
+        if (!isHashed) {
+            console.log('Pre-save hook - password modified and not hashed');
+            this.password = await bcrypt.hash(this.password, 10);
+        }
     }
     next();
 });
-module.exports = mongoose.model('User', userSchema)
+
+module.exports = mongoose.model('User', userSchema);
